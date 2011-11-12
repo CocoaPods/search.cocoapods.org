8b9a390df7651af80f3228c95c4329f835377d04
Florian Hanke <florian.hanke@gmail.com>
2011-11-12 19:51:40 +1100

    + SpecLoader -> Specs

--- lib/specs.rb
+++ lib/specs.rb
@@ -2,15 +2,22 @@ require 'net/http'
 
 class CocoapodSearch
 
-  class SpecLoader
+  class Specs
 
+    # Gets the latest master specs from the Specs repo.
+    #
+    # Note: Overwrites the old specs.zip.
+    #
     def get
       `wget http://github.com/CocoaPods/Specs/zipball/master -O specs.zip`
     end
 
+    # Prepares the specs for indexing.
+    #
     def prepare
+      `rm -rf specs`
       `unzip specs.zip -d specs`
-      `mv specs/CocoaPods-Specs-*/* specs/`
+      `mv -f specs/CocoaPods-Specs-*/* specs/`
       `rm -rf specs/CocoaPods-Specs-*`
     end
 
