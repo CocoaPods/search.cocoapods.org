7858cff16f08a18d2d94524d7d0352e27968a3ed
Florian Hanke <florian.hanke@gmail.com>
2012-02-06 12:15:06 +1100

    ! test URL, + specs

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -13,7 +13,7 @@ describe 'Integration Tests' do
     CocoapodSearch.prepare # Needed to load the data for the rendered search results.
   end
 
-  let(:pods) { Picky::TestClient.new(CocoapodSearch, :path => '/search/full') }
+  let(:pods) { Picky::TestClient.new(CocoapodSearch, :path => '/search') }
   
   # Testing a count of results.
   #
@@ -21,17 +21,34 @@ describe 'Integration Tests' do
 
   # Testing a specific order of result ids.
   #
-  it { pods.search('on:osx 1.0* k').ids.should == ['Kiwi'] }
+  it { pods.search('on:osx 1.0.0 k').ids.should == ['Kiwi'] }
 
   # Testing an order of result categories.
   #
   it { pods.search('on:osx k* a').should have_categories(['platform', 'name', 'author'], ['platform', 'name', 'summary']) }
   it { pods.search('on:osx jsonkit').should have_categories(['platform', 'name'], ['platform', 'dependencies']) }
 
+  # Category specific tests.
+  #
+
   # Similarity on author.
   #
   it { pods.search('on:ios thompsen~').ids.should == ['FormatterKit', 'TTTAttributedLabel'] }
-
+  
+  # Partial version search.
+  #
+  it { pods.search('on:osx kiwi 1').ids.should == ['Kiwi'] }
+  it { pods.search('on:osx kiwi 1.').ids.should == ['Kiwi'] }
+  it { pods.search('on:osx kiwi 1.0').ids.should == ['Kiwi'] }
+  it { pods.search('on:osx kiwi 1.0.').ids.should == ['Kiwi'] }
+  it { pods.search('on:osx kiwi 1.0.0').ids.should == ['Kiwi'] }
+  
+  # Platform constrained search.
+  #
+  it { pods.search('on:osx thompson').ids.should == ['FormatterKit'] }
+  it { pods.search('on:ios thompson').ids.should == ['FormatterKit', 'TTTAttributedLabel'] }
+  it { pods.search('on:osx on:ios thompson').ids.should == ['FormatterKit'] }
+  
   #
   # TODO We need specs. Lots of specs.
   #
