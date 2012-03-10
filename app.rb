ce0cae88d57df39f61288888687584cf2d9962c4
Florian Hanke <florian.hanke@gmail.com>
2012-03-10 23:46:30 +1100

    + analytics, + dry code

--- app.rb
+++ app.rb
@@ -207,6 +207,21 @@ class CocoapodSearch < Sinatra::Application
     def js path
       "<script src='javascripts/#{path}.js' type='text/javascript'></script>"
     end
+    
+    def analytics
+<<-ANALYTICS
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-29866548-1']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+ANALYTICS
+    end
 
   end
 
