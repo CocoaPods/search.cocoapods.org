e3c7df174528570954d70345bd7aef51cf283c7b
Fabio Pelosin <fabiopelosin@gmail.com>
2012-07-04 14:33:31 +0200

    Use the master branch of the specs repo.

--- lib/specs.rb
+++ lib/specs.rb
@@ -15,7 +15,7 @@ class CocoapodSearch
     # Note: Overwrites the old specs.zip.
     #
     def get
-      `curl -L -o ./tmp/specs.tar.gz http://github.com/CocoaPods/Specs/tarball/0.6`
+      `curl -L -o ./tmp/specs.tar.gz http://github.com/CocoaPods/Specs/tarball/master`
     end
 
     # Prepares the specs for indexing.
