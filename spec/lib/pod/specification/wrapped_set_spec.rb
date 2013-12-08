75b487d373b476dd0cecea2abd9eb93e7253a642
Florian R. Hanke <florian.hanke@gmail.com>
2013-12-08 08:00:18 +0100

    Rename and adapt spec.

--- spec/lib/pod/specification/wrapped_set_spec.rb
+++ spec/lib/pod/specification/wrapped_set_spec.rb
@@ -2,11 +2,11 @@
 #
 require 'spec_helper'
 
-describe Pod::Specification::Set do
+describe Pod::Specification::WrappedSet do
   
   context 'simple example' do
 
-    let(:set) { described_class.new 'ABCSomeName' }
+    let(:set) { described_class.new Pod::Specification::Set.new 'ABCSomeName' }
   
     it 'splits the set name correctly' do
       set.split_name.should == ["ABCSomeName", "abc", "some", "", "name", "somename"]
