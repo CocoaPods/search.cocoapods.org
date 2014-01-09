a4144154882c8541a55d9024099fb254d70d8a9c
Florian R. Hanke <florian.hanke@gmail.com>
2014-01-09 15:56:01 +0100

    Remove unicorn.rb.

--- unicorn.rb
+++
@@ -1,7 +0,0 @@
-listen            '0.0.0.0:8080'
-pid               'tmp/pids/unicorn.pid'
-preload_app       true
-stderr_path       'log/unicorn.stderr.log'
-stdout_path       'log/unicorn.stdout.log'
-timeout           10
-worker_processes  3
