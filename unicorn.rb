d0934a54885f37781f00a3ed2491cfea5b101c79
Florian R. Hanke <florian.hanke@gmail.com>
2014-01-28 13:11:09 +0100

    Try ./log/...

--- unicorn.rb
+++ unicorn.rb
@@ -1,6 +1,6 @@
 pid               'tmp/pids/unicorn.pid'
 preload_app       true
-stderr_path       'tmp/unicorn.stderr.log'
-stdout_path       'tmp/unicorn.stdout.log'
+stderr_path       './log/unicorn.stderr.log'
+stdout_path       './log/unicorn.stdout.log'
 # timeout           10 # We don't have a timeout to allow pre-reindexing in a child.
 worker_processes  4
