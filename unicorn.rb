c96d257fdb7430ce421ba1ad1b0e2d9938c06b75
Florian R. Hanke <florian.hanke@gmail.com>
2014-01-28 13:22:11 +0100

    We've gone over the memory limit. Reducing amount of workers.

--- unicorn.rb
+++ unicorn.rb
@@ -3,4 +3,4 @@ preload_app       true
 stderr_path       'tmp/unicorn.stderr.log'
 stdout_path       'tmp/unicorn.stdout.log'
 # timeout           10 # We don't have a timeout to allow pre-reindexing in a child.
-worker_processes  4
+worker_processes  3
