ecf98effd2b31f2f2461b38cc6ff8f79f1c0422f
Florian R. Hanke <florian.hanke@gmail.com>
2014-06-24 20:31:52 +0200

    For now, comment obsolete code.

--- lib/pod/specification.rb
+++ lib/pod/specification.rb
@@ -1,28 +1,28 @@
-module Pod
-
-  class Specification
-    
-    # Returns a list of all subspecs.
-    #
-    # TODO Eventually use the method supplied by CocoaPods
-    #
-    def recursive_subspecs
-      mapper = lambda do |spec|
-        spec.subspecs.map do |subspec|
-          [subspec, *mapper.call(subspec)]
-        end.flatten
-      end
-
-      mapper.call self
-    rescue StandardError
-      []
-    end
-
-    # Ignore invalid assignments in podspecs.
-    #
-    def method_missing name, *args, &block
-      super unless name[-1..-1] == '='
-    end
-
-  end
-end
+# module Pod
+#
+#   class Specification
+#
+#     # Returns a list of all subspecs.
+#     #
+#     # TODO Eventually use the method supplied by CocoaPods
+#     #
+#     def recursive_subspecs
+#       mapper = lambda do |spec|
+#         spec.subspecs.map do |subspec|
+#           [subspec, *mapper.call(subspec)]
+#         end.flatten
+#       end
+#
+#       mapper.call self
+#     rescue StandardError
+#       []
+#     end
+#
+#     # Ignore invalid assignments in podspecs.
+#     #
+#     def method_missing name, *args, &block
+#       super unless name[-1..-1] == '='
+#     end
+#
+#   end
+# end
