290cdd2ef6f10e9894503bb6ca52bfedf3ed2448
Florian R. Hanke <florian.hanke@gmail.com>
2014-06-24 20:36:36 +0200

    Comment WrappedSet specs.

--- spec/lib/pod/specification/wrapped_set_spec.rb
+++ spec/lib/pod/specification/wrapped_set_spec.rb
@@ -1,19 +1,19 @@
-# coding: utf-8
+# # coding: utf-8
+# #
+# require File.expand_path '../../../../spec_helper', __FILE__
 #
-require File.expand_path '../../../../spec_helper', __FILE__
-
-describe Pod::Specification::WrappedSet do
-  
-  describe 'simple example' do
-    
-    def set
-      @set ||= Pod::Specification::WrappedSet.new Pod::Specification::Set.new 'ABCSomeName+OtherName-Abc+Def'
-    end
-  
-    it 'splits the set name correctly' do
-      set.split_name.should == ["abcsomename+othername-abc+def", "abc", "somename", "abcsomename", "+", "othername", "-", "def", "some", "", "name", "other"]
-    end
-
-  end
-  
-end
+# describe Pod::Specification::WrappedSet do
+#
+#   describe 'simple example' do
+#
+#     def set
+#       @set ||= Pod::Specification::WrappedSet.new Pod::Specification::Set.new 'ABCSomeName+OtherName-Abc+Def'
+#     end
+#
+#     it 'splits the set name correctly' do
+#       set.split_name.should == ["abcsomename+othername-abc+def", "abc", "somename", "abcsomename", "+", "othername", "-", "def", "some", "", "name", "other"]
+#     end
+#
+#   end
+#
+# end
