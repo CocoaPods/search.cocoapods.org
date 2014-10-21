6028873f36021621f5add3537db6ca971a77dad0
Florian R. Hanke <florian.hanke@gmail.com>
2014-10-21 13:37:22 +0200

    Remove unnecessary models.

--- lib/models/version.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path '../commit', __FILE__
-
-# Only for reading purposes.
-#
-class Version < Sequel::Model(:pod_versions)
-  many_to_one :pod
-  one_to_many :commits, :key => :pod_version_id
-end
