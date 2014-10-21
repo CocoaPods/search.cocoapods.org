8b2ce5e55ebd16d16a30affde98c458502692164
Florian R. Hanke <florian.hanke@gmail.com>
2014-10-21 13:36:56 +0200

    Replace models by domain/db.

--- lib/models.rb
+++
@@ -1,5 +0,0 @@
-ENV['RACK_ENV'] ||= 'production'
-ENV['DATABASE_URL'] ||= "postgres://localhost/trunk_cocoapods_org_#{ENV['RACK_ENV']}"
-DB = Sequel.connect ENV['DATABASE_URL']
-
-require File.expand_path '../models/pod', __FILE__
