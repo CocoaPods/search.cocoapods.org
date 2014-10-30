5792eb39f918ef9f6c0b5fa1f8cbc7acb7036ed2
Florian R. Hanke <florian.hanke@gmail.com>
2014-10-30 21:18:55 +0100

    Fix specs.

--- spec/special_cases_spec.rb
+++ spec/special_cases_spec.rb
@@ -10,17 +10,21 @@ describe 'Special Cases' do
   def special_cases
     Picky::TestClient.new CocoapodSearch, :path => '/api/v1/pods.flat.ids.json'
   end
+
+  it 'will default to name with unrecognized sort orders' do
+    special_cases.search('test', sort: 'quack').should == ["ABTest", "ABTestingVessel", "AGAsyncTestHelper", "AGWaitForAsyncTestHelper", "AOTestCase", "AsyncSenTest", "AsyncTestSupporter", "BenchmarkTestCase", "DBMigrationTestKit", "EXPMatchers+FBSnapshotTest", "FBSnapshotTestCase", "FOTestKit", "FSJUnitTestLog", "GenerateTestCoverageFor_iOS7", "GoogleTest", "IFATestingSupport", "JSON-Schema-Test-Suite", "KNMParametrizedTests", "KPAViewControllerTestHelper", "Keystone-TestData-iOS"]
+  end
   
   it 'will correctly find _.m' do
     special_cases.search('_.m').should == ['_.m']
   end
   
   it 'will correctly find JSONKit' do
-    special_cases.search('JSONKit very high library').should == ["JSONKit", "JSONKit-NoWarning"]
+    special_cases.search('JSONKit very high library', sort: 'name').should == ["JSONKit", "JSONKit-NoWarning"]
   end
   
   it 'will correctly find something split on @' do
-    special_cases.search('name:KGNoise').should == ["KGNoise", "KGNoise@tonyzonghui"]
+    special_cases.search('name:KGNoise', sort: 'name').should == ["KGNoise", "KGNoise@tonyzonghui"]
     special_cases.search('name:KGNoise@tonyzonghui').should == ['KGNoise@tonyzonghui']
   end
   
@@ -32,7 +36,7 @@ describe 'Special Cases' do
   end
   
   it 'will correctly find something split on -' do
-    special_cases.search('name:mkmapview"').should == ["MKMapView+AttributionView", "MKMapView-Blocks", "MKMapView-ZoomLevel"]
+    special_cases.search('name:mkmapview"', sort: 'name').should == ["MKMapView+AttributionView", "MKMapView-Blocks", "MKMapView-ZoomLevel"]
     special_cases.search('name:AttributionView"').should == ['MKMapView+AttributionView']
     special_cases.search('name:mkmapview name:attributionview').should == ['MKMapView+AttributionView']
   end
