40b4c378dd66f5091f761141525e90699cda5297
Florian R. Hanke <florian.hanke@gmail.com>
2014-10-31 09:06:48 +0100

    Set up multiple channels.

--- unicorn.rb
+++ unicorn.rb
@@ -1,15 +1,20 @@
+number_of_worker_processes = 3
+
 pid               'tmp/pids/unicorn.pid'
 preload_app       true
 stderr_path       'tmp/unicorn.stderr.log'
 stdout_path       'tmp/unicorn.stdout.log'
 timeout           10
-worker_processes  3
+worker_processes  number_of_worker_processes
 
 before_fork do |_, _|
-  Channel.instance # For communication between n worker - 1 search engine processes.
+  # For communication between n worker - 1 search engine processes.
+  Channel.instance.master_setup_number_of_children number_of_worker_processes
 end
 
 after_fork do |server, worker|
+  Channel.instance.child_choose_channel worker.nr
+  
   # Load the DB after forking.
   #
   require File.expand_path '../lib/database', __FILE__
