f461776749fcacc45d57037324590ce82e93ed75
Florian R. Hanke <florian.hanke@gmail.com>
2014-11-03 20:55:13 +0100

    Adapt specs to popularity.

--- spec/api/facets_integration_spec.rb
+++ spec/api/facets_integration_spec.rb
@@ -18,7 +18,7 @@ describe 'Search Integration Tests' do
 
   ok do
     get '/api/v1/pods.facets.json',  only: 'platform'
-    Yajl.load(last_response.body).should == {"platform"=>{"ios"=>87, "osx"=>23}}
+    Yajl.load(last_response.body).should == {"platform"=>{"ios"=>168, "osx"=>42}}
   end
 
   ok do
@@ -28,22 +28,22 @@ describe 'Search Integration Tests' do
 
   ok do
     get '/api/v1/pods.facets.json',  except: 'tags'
-    Yajl.load(last_response.body).should == {"platform"=>{"ios"=>87, "osx"=>23}}
+    Yajl.load(last_response.body).should == {"platform"=>{"ios"=>168, "osx"=>42}}
   end
 
   ok do
     get '/api/v1/pods.facets.json',  only: 'platform', filter: 'mattt'
-    Yajl.load(last_response.body).should == {"platform"=>{"ios"=>8, "osx"=>7}}
+    Yajl.load(last_response.body).should == {"platform"=>{"ios"=>6, "osx"=>5}}
   end
 
   ok do
     get '/api/v1/pods.facets.json',  only: %w(platform name), include: 'name', filter: 'author:mattt'
-    Yajl.load(last_response.body).should == {"platform"=>{"ios"=>8, "osx"=>7}, "name"=>{"activity"=>1, "image"=>1, "request"=>3, "core"=>1, "network"=>1, "af"=>7, "client"=>1, "operation"=>3, "afcoreimageresponseserializer"=>1, "coreimageresponseserializer"=>1, "response"=>1, "serializer"=>1, "afhttp"=>1, "logger"=>2, "afhttprequestoperationlogger"=>1, "requestoperationlogger"=>1, "afincrementalstore"=>1, "incrementalstore"=>1, "incremental"=>1, "store"=>1, "afjsonrpcclient"=>1, "afjsonrpc"=>1, "afkissxmlrequestoperation"=>2, "kissxmlrequestoperation"=>2, "kiss"=>2, "xml"=>2, "afkissxmlrequestoperation@aceontech"=>1, "aceontech"=>1, "afmsgpackserialization"=>1, "msgpackserialization"=>1, "msg"=>1, "pack"=>1, "serialization"=>1, "afnetworkactivitylogger"=>1, "networkactivitylogger"=>1, "afnetworking"=>1, "networking"=>1}}
+    Yajl.load(last_response.body).should == {"platform"=>{"ios"=>6, "osx"=>5}, "name"=>{"afnetworking"=>1, "af"=>2, "networking"=>1, "kit"=>1, "tttattributedlabel"=>1, "ttt"=>1, "attributedlabel"=>1, "attributed"=>1, "label"=>1, "formatterkit"=>1, "formatter"=>1, "afincrementalstore"=>1, "incrementalstore"=>1, "incremental"=>1, "store"=>1, "control"=>1, "groundcontrol"=>1, "ground"=>1, "cargobay"=>1, "cargo"=>1, "bay"=>1, "ono"=>1}}
   end
 
   ok do
     get '/api/v1/pods.facets.json',  include: 'version', filter: 'mattt'
-    Yajl.load(last_response.body).should == {"platform"=>{"ios"=>8, "osx"=>7}, "tags"=>{"image"=>1, "http"=>1, "network"=>2, "logging"=>2, "json"=>1, "client"=>1, "xml"=>2}, "version"=>{"2.0.0"=>2, "2.0.1"=>2, "1.0"=>1, "1.0.1"=>1, "2.0.2"=>2, "0.0.1"=>3, "0.1.0"=>2, "1.0.0"=>2, "1.1.1"=>1, "1.2.0"=>1, "2.0.3"=>1, "1.2.1"=>1, "1.1.0"=>1, "0.9.2"=>1, "0.0.4"=>1, "1.3.0"=>1, "1.3.1"=>1, "2.1.0"=>1, "0.4.0"=>2, "0.3.0"=>2, "0.5.0"=>1, "0.9.0"=>2, "0.10.0"=>2, "0.3.2"=>1, "0.5.1"=>2, "0.3.1"=>2, "0.4.1"=>1, "2.3.0"=>1, "2.2.1"=>1, "0.10.1"=>1, "1.0rc2"=>1, "rc3"=>1, "2.3.1"=>1, "1.3.4"=>1, "1.0rc3"=>1, "2.2.4"=>1, "0.7.0"=>1, "2.2.0"=>1, "2.2.2"=>1, "rc1"=>1, "1.3.2"=>1, "2.2.3"=>1, "1.0rc1"=>1, "1.3.3"=>1, "rc2"=>1, "0.9.1"=>1}}
+    Yajl.load(last_response.body).should == {"platform"=>{"ios"=>6, "osx"=>5}, "tags"=>{"network"=>1, "xml"=>1}, "version"=>{"2.3.0"=>1, "0.9.0"=>1, "2.2.1"=>1, "0.10.1"=>1, "2.0.3"=>2, "1.0rc2"=>1, "2.1.0"=>3, "1.0"=>1, "2.0.0"=>3, "rc3"=>1, "1.3.0"=>3, "2.3.1"=>1, "0.10.0"=>2, "1.3.4"=>1, "1.0rc3"=>1, "2.2.4"=>1, "1.3.1"=>2, "2.0.1"=>3, "1.1.1"=>3, "0.7.0"=>2, "2.2.0"=>1, "1.0.1"=>3, "2.2.2"=>1, "rc1"=>1, "2.0.2"=>2, "1.3.2"=>2, "2.2.3"=>1, "1.0rc1"=>1, "1.3.3"=>1, "rc2"=>1, "0.9.2"=>1, "1.2.0"=>3, "0.5.1"=>2, "1.2.1"=>3, "0.9.1"=>1, "1.1.0"=>4, "1.0.0"=>4, "0.0.2"=>2, "0.1.0"=>2, "0.0.1"=>2, "1.8.0"=>1, "1.8.1"=>1, "1.5.0"=>2, "1.7.1"=>1, "1.7.0"=>1, "1.7.2"=>1, "1.9.4"=>1, "1.9.5"=>1, "0.6.0"=>1, "1.4.0"=>2, "0.5.0"=>1, "1.6.0"=>1, "1.5.1"=>2, "1.4.2"=>1, "1.6.4"=>1, "1.2.2"=>1, "1.6.3"=>1, "1.9.0"=>1, "1.6.1"=>1, "1.6.2"=>1, "1.7.4"=>1, "0.3.1"=>2, "0.2.1"=>1, "0.3.0"=>2, "0.0.4"=>1, "0.0.3"=>1, "0.0.5"=>1, "0.3.2"=>2, "0.4.0"=>1, "0.3.3"=>1, "0.2.0"=>1, "1.9.2"=>1, "1.7.3"=>1, "1.9.3"=>1, "1.10.0"=>1, "1.9.1"=>1, "1.7.5"=>1, "1.4.1"=>2, "1.10.1"=>1, "1.4.3"=>1, "1.1.2"=>1, "0.4.1"=>1}}
   end
 
   # ok do
