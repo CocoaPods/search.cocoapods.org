615fc05ffa9a7c479978f97a5265965ae7086f68
Florian R. Hanke <florian.hanke@gmail.com>
2014-11-06 21:07:08 +0100

    Start adding pod specs.

--- spec/lib/models/pod_spec.rb
+++ spec/lib/models/pod_spec.rb
@@ -2,20 +2,102 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../../../../lib/models/pod', __FILE__)
 
 describe Pod do
-
-  # TODO: Rewrite.
-  #
-  # describe '#specification_json' do
-  #   before do
-  #     @pod = Domain.pods.insert(:name => 'TestPod1')
-  #     version = @pod.add_version(:name => '1.10.0')
-  #     version.add_commit(:sha => 'shalalalala', :committer_id => 1, :specification_data => 'data10')
-  #     version = @pod.add_version(:name => '1.9.0')
-  #     version.add_commit(:sha => 'shalalalala', :committer_id => 1, :specification_data => 'data9')
-  #   end
-  #   it 'returns the data of the last version' do
-  #     @pod.specification_json.should == 'data10'
-  #   end
-  # end
-
+  describe 'AFNetworking' do
+    
+    def specification
+      Hashie::Mash.new(
+        "name"=>"AFNetworking",
+        "version"=>"2.3.1",
+        "license"=>"MIT",
+        "summary"=>"A delightful iOS and OS X networking framework.",
+        "homepage"=>"https://github.com/AFNetworking/AFNetworking",
+        "social_media_url"=>"https://twitter.com/AFNetworking",
+        "authors"=>{"Mattt Thompson"=>"m@mattt.me"},
+        "source"=>{
+          "git"=>"https://github.com/AFNetworking/AFNetworking.git",
+          "tag"=>"2.3.1",
+          "submodules"=>true},
+          "requires_arc"=>true,
+          "platforms"=>{
+            "ios"=>"6.0",
+            "osx"=>"10.8"
+          },
+          "public_header_files"=>"AFNetworking/*.h",
+          "source_files"=>"AFNetworking/AFNetworking.h",
+          "subspecs"=>[
+            {
+              "name"=>"Serialization",
+              "source_files"=>"AFNetworking/AFURL{Request,Response}Serialization.{h,m}",
+              "ios"=>{
+                "frameworks"=>[
+                  "MobileCoreServices",
+                  "CoreGraphics"
+                ]
+              },
+              "osx"=>{
+                "frameworks"=>"CoreServices"
+              }
+            },
+            {
+              "name"=>"Security",
+              "source_files"=>"AFNetworking/AFSecurityPolicy.{h,m}",
+              "frameworks"=>"Security"}, {"name"=>"Reachability",
+              "source_files"=>"AFNetworking/AFNetworkReachabilityManager.{h,m}",
+              "frameworks"=>"SystemConfiguration"
+            },
+            {
+              "name"=>"NSURLConnection",
+              "dependencies"=>{
+                "AFNetworking/Serialization"=>[],
+                "AFNetworking/Reachability"=>[],
+                "AFNetworking/Security"=>[]
+              },
+              "source_files"=>[
+                "AFNetworking/AFURLConnectionOperation.{h,m}",
+                "AFNetworking/AFHTTPRequestOperation.{h,m}",
+                "AFNetworking/AFHTTPRequestOperationManager.{h,m}"
+              ]
+            },
+            {
+              "name"=>"NSURLSession",
+              "dependencies"=>{
+                "AFNetworking/Serialization"=>[],
+                "AFNetworking/Reachability"=>[],
+                "AFNetworking/Security"=>[]
+              },
+              "source_files"=>[
+                "AFNetworking/AFURLSessionManager.{h,m}",
+                "AFNetworking/AFHTTPSessionManager.{h,m}"
+              ]
+            },
+            {
+              "name"=>"UIKit",
+              "platforms"=>{
+                "ios"=>"6.0"
+              },
+              "dependencies"=>{
+                "AFNetworking/NSURLConnection"=>[],
+                "AFNetworking/NSURLSession"=>[]
+              },
+              "ios"=>{
+                "public_header_files"=>"UIKit+AFNetworking/*.h",
+                "source_files"=>"UIKit+AFNetworking"
+              },
+              "osx"=>{
+                "source_files"=>""
+              }
+            }
+          ]
+        )
+    end
+    
+    def pod
+      Pod.new(specification)
+    end
+  
+    ok { pod.name.should == 'AFNetworking' }
+    ok { pod.split_name.should == ['afnetworking', 'af', 'networking'] }
+    ok { pod.split_name_for_automatic_splitting.should == ['networking'] }
+    
+  end
 end
