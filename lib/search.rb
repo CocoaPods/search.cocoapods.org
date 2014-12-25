f177d9e1ace5f973fcbef05cc56c73b84566eec6
Samuel E. Giddins <segiddins@segiddins.me>
2014-12-25 12:08:55 -0300

    Add indexing on subspec names

--- lib/search.rb
+++ lib/search.rb
@@ -109,6 +109,10 @@ class Search
                partial: no_partial,
                qualifiers: [:tag, :tags],
                tokenize: false
+      category :subspecs,
+               partial: full_partial,
+               qualifiers: %i(subspec subspecs),
+               from: :mapped_subspec_names
     end
 
     # Define a search over the books index.
@@ -131,6 +135,7 @@ class Search
             [:name, :summary] => -3,
             [:summary]        => -3,
             [:dependencies]   => -4,
+            [:subspecs]       => -6,
             [:platform, :name, :author]  => +2,
             [:platform, :name]           => +3,
             [:platform, :tags]           => +1,
@@ -176,7 +181,7 @@ class Search
 
     @facet_keys = @index.categories.map(&:name).sort - [:id, :name, :author,
                                                         :summary, :version,
-                                                        :dependencies]
+                                                        :dependencies, :subspecs]
   end
 
   # Reindex all pods.
