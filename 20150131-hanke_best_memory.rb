e375bdd84cb04afbbbd4756f53f9c879b72e209b
Florian R. Hanke <florian.hanke@gmail.com>
2015-01-31 17:29:27 +0100

    Add best memory water level.

---
+++ 20150131-hanke_best_memory.rb
@@ -0,0 +1,6 @@
+368748 ruby /Users/hanke/.gem/ruby/2.2.0/bin/unicorn -p 5000 -c unicorn.rb
+ 70476 unicorn master -p 5000 -c unicorn.rb     
+ 20084 ruby /Users/hanke/.gem/ruby/2.2.0/bin/unicorn -p 5000 -c unicorn.rb
+ 15852 unicorn worker[1] -p 5000 -c unicorn.rb     
+ 15144 unicorn worker[0] -p 5000 -c unicorn.rb     
+   336 grep unicorn
