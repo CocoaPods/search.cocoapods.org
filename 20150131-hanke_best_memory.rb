f71d742a52191125754896fb8af63a4f07dbcd19
Florian R. Hanke <florian.hanke@gmail.com>
2015-01-31 17:34:16 +0100

    Add notes.

--- 20150131-hanke_best_memory.rb
+++ 20150131-hanke_best_memory.rb
@@ -1,6 +1,7 @@
-368748 ruby /Users/hanke/.gem/ruby/2.2.0/bin/unicorn -p 5000 -c unicorn.rb
- 70476 unicorn master -p 5000 -c unicorn.rb     
- 20084 ruby /Users/hanke/.gem/ruby/2.2.0/bin/unicorn -p 5000 -c unicorn.rb
- 15852 unicorn worker[1] -p 5000 -c unicorn.rb     
- 15144 unicorn worker[0] -p 5000 -c unicorn.rb     
-   336 grep unicorn
+ index: 368748 ruby /Users/hanke/.gem/ruby/2.2.0/bin/unicorn -p 5000 -c unicorn.rb
+master:  70476 unicorn master -p 5000 -c unicorn.rb
+ stats:  20084 ruby /Users/hanke/.gem/ruby/2.2.0/bin/unicorn -p 5000 -c unicorn.rb
+   web:  15852 unicorn worker[1] -p 5000 -c unicorn.rb
+   web:  15144 unicorn worker[0] -p 5000 -c unicorn.rb
+   
+* Could we reduce the master size?
